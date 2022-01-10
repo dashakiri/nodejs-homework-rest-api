d53ce0674ef610b7e2fc22f31978869a519f9a64
Dasha Kirienko <dkirienko25@gmail.com>
2022-01-10 13:34:12 +0300

    change node command

--- bin/server.js
+++ bin/server.js
@@ -2,11 +2,10 @@ const app = require('../app')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-// const PORT = process.env.PORT || 3000
-const { DB_HOST } = process.env
+const { DB_HOST, PORT = 3000 } = process.env
 
-app.listen({ port: (process.env.PORT || 3000) }, () => {
-  console.log(`Server running. Use our API on port: ${ port }`)
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
 })
 
 mongoose.connect(DB_HOST)
